bf8739d16ef3c43a3fc286baa7ceae14592acb64
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2021-08-13 09:41:30 -0500

    changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,24 @@
+#roles insertion
+INSERT INTO roles (name) VALUES ('ADMIN');
+INSERT INTO roles (name) VALUES ('SELLER');
+INSERT INTO roles (name) VALUES ('BUYER');
+
+#product categories insertion
+INSERT INTO categories (name) VALUES ('Food');
+INSERT INTO categories (name) VALUES ('Toys');
+INSERT INTO categories (name) VALUES ('Electronics');
+INSERT INTO categories (name) VALUES ('Fashion');
+INSERT INTO categories (name) VALUES ('Furniture');
+
+#users insertions
+
 INSERT INTO `users` (`id`, `city`, `country`, `state`, `street`, `zipcode`, `email`, `enabled`, `first_name`, `last_name`, `password`, `phone_number`, `photo`, `username`) VALUES
 (1, NULL, NULL, NULL, NULL, NULL, 'admin@example.com', b'1', 'Admin', 'Admin', '$2a$10$JirblkU.nrFQVWpRJkeTcev6J1I/UoZvIDdjPUiIHKAeBrtZq8iYe', NULL, NULL, 'admin'),
 (2, NULL, NULL, NULL, NULL, NULL, NULL, b'1', 'Seller', 'Seller', '$2a$10$JirblkU.nrFQVWpRJkeTcev6J1I/UoZvIDdjPUiIHKAeBrtZq8iYe', NULL, NULL, 'seller'),
 (3, NULL, NULL, NULL, NULL, NULL, NULL, b'1', 'Buyer', 'Buyer', '$2a$10$JirblkU.nrFQVWpRJkeTcev6J1I/UoZvIDdjPUiIHKAeBrtZq8iYe', NULL, NULL, 'buyer'),
 (5, NULL, NULL, NULL, NULL, NULL, 'test@example.com', b'1', 'Test', 'Test', '$2a$10$2ct/ZFJZYvsYFD6MlMpyXuzDyoO1xWrpfDcH8pNGE9h13AMMZORPu', '5043446969', NULL, 'test@example.com');
 
-INSERT INTO roles (name) VALUES ('ADMIN');
-INSERT INTO roles (name) VALUES ('SELLER');
-INSERT INTO roles (name) VALUES ('BUYER');
-
+#user roles insertion
 INSERT INTO USERS_ROLES(user_id,role_id)
 SELECT (SELECT ID FROM USERS WHERE USERNAME='ADMIN' LIMIT 1) , (SELECT ID FROM ROLES WHERE NAME='ADMIN' LIMIT 1);
 INSERT INTO USERS_ROLES(user_id,role_id)
@@ -15,8 +26,9 @@ SELECT (SELECT ID FROM USERS WHERE USERNAME='SELLER' LIMIT 1) , (SELECT ID FROM 
 INSERT INTO USERS_ROLES(user_id,role_id)
 SELECT (SELECT ID FROM USERS WHERE USERNAME='BUYER' LIMIT 1) , (SELECT ID FROM ROLES WHERE NAME='BUYER' LIMIT 1);
 
-INSERT INTO categories (name) VALUES ('Fashion');
-INSERT INTO categories (name) VALUES ('Toys');
-INSERT INTO categories (name) VALUES ('Electronics');
-INSERT INTO categories (name) VALUES ('Food');
-INSERT INTO categories (name) VALUES ('Furniture');
+#Buyer insertion
+
+#Seller insertion
+
+#Admin insertion
+
